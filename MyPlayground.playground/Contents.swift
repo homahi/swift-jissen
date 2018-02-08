828eae16d305a71f19bb96611f9ac75ef0464884
homahi <homahi0128@gmail.com>
2018-02-08 15:51:59 +0900

    init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+class User {
+    let id: Int
+    
+    var message: String {
+        return "Hello"
+    }
+    
+    init(id: Int) {
+        self.id = id
+    }
+    
+    func printProfile() {
+        print("id: \(id)")
+        print("message: \(message)")
+    }
+}
+
+class RegisteredUser: User {
+    let name: String
+    
+    init(id: Int, name: String) {
+        self.name = name
+        super.init(id: id)
+    }
+}
+
+let registeredUser = RegisteredUser(id: 1, name: "Yosuke Ishikawa")
+let id = registeredUser.id
+let message = registeredUser.message
+registeredUser.printProfile()
